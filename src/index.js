7863b8eccae1ea8da0910024f4b4998b4fdfd42c
Aleks Davydchenko <slim_n@mail.ru>
2021-12-06 23:57:11 +0300

    +webpack new rules

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
 import "./styles/normalize.min.scss"
 import "./styles/style.scss";
-
+import "./img/logo.svg";
 
--- webpack.config.js
+++ webpack.config.js
@@ -5,29 +5,47 @@ const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 module.exports = {
   mode: 'development',
+  entry:{
+    index:"/src/index.js",
+  },
+  devtool: "inline-source-map",
+  devServer: {
+    static: "./dist",
+  },
   output: {
-    assetModuleFilename: 'img/[hash][ext][query]'
+    filename:"js/[name].bundel.js",
+    clean: true,
   },
   plugins: [
     new HtmlWebpackPlugin({
       template: "./src/index.pug",
       favicon: "./src/img/favicon.ico",
     }),
-    new MiniCssExtractPlugin(),
+    new MiniCssExtractPlugin(
+      {
+        filename: "css/[name].css"
+      }
+    ),
   ],
   module: {
     rules: [
       {
-        test: /\.(png|jpg|gif)$/i,
-        type: 'asset/resource'
-      },
-      {
-        test: /\.(svg)$/i,
-        type: 'asset/inline'
+        test: /\.(png|jpe?g|gif|svg|ico)$/i,
+        type: 'asset/resource',
+        generator: {
+          filename: "images/[name][ext][query]"
+        },
       },
       {
         test: /\.s[ac]ss$/i,
         use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"],
+      },
+      {
+      test: /\.(woff|woff2|eot|ttf|otf)$/i,
+      type: 'asset/resource',
+      generator: {
+        filename: "fonts/[name][ext][query]"
+      },
       },
       {
         test: /\.pug$/,
